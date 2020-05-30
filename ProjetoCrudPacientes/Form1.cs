8f02d71d75ad320f71507ec195a106699b3e6c18
jmauricio2019 <54713911+jmauricio2019@users.noreply.github.com>
2020-05-30 10:38:16 -0300

    Primeiro

---
+++ ProjetoCrudPacientes/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoCrudPacientes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                progressBar1.Value = progressBar1.Value + 2;
+            }
+            catch
+            {
+                timer1.Enabled = false;
+
+                this.Visible = false;
+
+               frmOpcoes frm = new frmOpcoes();
+
+                frm.ShowDialog();
+
+            }
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ProjetoCrudPacientes/Frmopcoes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoCrudPacientes
+{
+    public partial class frmOpcoes : Form
+    {
+        public frmOpcoes()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnCadastrodeClientes_Click(object sender, EventArgs e)
+        {
+            frmCadPacientes frm = new frmCadPacientes();
+            frm.ShowDialog();
+        }
+    }
+}
