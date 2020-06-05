d01f879211e793d17893512373fac4a5be0373e1
jmauricio2019 <54713911+jmauricio2019@users.noreply.github.com>
2020-06-05 17:46:29 -0300

    atualizado enter da pesquisa

--- ProjetoCrudPacientes/frmCadPacientes.cs
+++ ProjetoCrudPacientes/frmCadPacientes.cs
@@ -999,79 +999,15 @@ namespace ProjetoCrudPacientes
         {
             if (e.KeyChar == 13)
             {
-                int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
-                int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
-                string cpf = txtCpf1.Text;
-                cpf = cpf.Trim().Replace(".", "").Replace("-", "");
-                char[] arr;
-
-                arr = cpf.ToCharArray();
-                //verifica se todos os números são iguais para validar cpf
-                int cont = 0;
-                for (int i = 0; i < arr.Length - 1; i++)
-                {
-                    if (arr[i] == arr[i + 1])
-                    {
-                        cont++;
-                    }
-                }
-                if (arr.Length == 11)
-                {
-                    int soma = 0;
-
-                    for (int i = 0; i < 9; i++)
-                        soma += int.Parse(arr[i].ToString()) * multiplicador1[i];
-
-                    int resto = soma % 11;
-                    if (resto < 2)
-                        resto = 0;
-                    else
-                        resto = 11 - resto;
-
-
-
-                    soma = 0;
-                    for (int i = 0; i < 10; i++)
-                        soma += int.Parse(arr[i].ToString()) * multiplicador2[i];
-
-                    int resto1 = soma % 11;
-                    if (resto1 < 2)
-                        resto1 = 0;
-                    else
-                        resto1 = 11 - resto1;
-
-                    if (resto == int.Parse(arr[9].ToString()) && resto1 == int.Parse(arr[10].ToString()) && cont != 10)
-                    {
-                        txtCpf1.BackColor = Color.White;
-                        txtCpf1.TextAlign = HorizontalAlignment.Left;
-                        Selecionar();
-                        LimparBox2();
-                    }
-                    else
-                    {
-                        MessageBox.Show("CPF Inválido");
-
-                        txtCpf1.BackColor = Color.Red;
-                        txtCpf1.TextAlign = HorizontalAlignment.Left;
-                        txtCpf1.Focus();
-                    }
-
-
-                }
-                else
-                {
-                    MessageBox.Show(" Faltam números no CPF");
-                }
-
+                SelecionarDataGrewView();
             }
-
         }
 
         private void txtNome1_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (e.KeyChar == 13)
             {
-                Selecionar();
+                SelecionarDataGrewView();
             }
         }
 
@@ -1079,7 +1015,7 @@ namespace ProjetoCrudPacientes
         {
             if (e.KeyChar == 13)
             {
-                Selecionar();
+                SelecionarDataGrewView();
             }
         }
 
@@ -1087,7 +1023,7 @@ namespace ProjetoCrudPacientes
         {
             if (e.KeyChar == 13)
             {
-                Selecionar();
+                SelecionarDataGrewView();
             }
         }
 
